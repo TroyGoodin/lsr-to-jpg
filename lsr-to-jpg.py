55a50a8eda0b104c863baab97f593e93756ddec9
tagboy07 <tagboy07@gmail.com>
2020-11-29 13:17:55 -0800

    Add option to generate a jpg for every combination of layers from lsr

--- lsr-to-jpg.py
+++ lsr-to-jpg.py
@@ -1,13 +1,14 @@
 import click
 import errno
+import more_itertools
 import json
 import os
 import shutil
 import zipfile
 from PIL import Image
 
-EXTRACTED_CONTENT_PATH = 'extracted-content'
 OUTPUT_PATH = 'output'
+EXTRACTED_CONTENT_PATH = 'extracted-lsr-content'
 
 
 def extract_lsr_content(input_file_path, extracted_content_path=EXTRACTED_CONTENT_PATH):
@@ -22,39 +23,48 @@ def json_file_to_object(path):
     return json.loads(data)
 
 
-def generate_image(extracted_content_path=EXTRACTED_CONTENT_PATH, output_path=OUTPUT_PATH):
-    lsr_content = json_file_to_object(
-        f"{extracted_content_path}/Contents.json")
+def generate_images(extracted_content_path=EXTRACTED_CONTENT_PATH, output_path=OUTPUT_PATH, all_combinations=False,
+                    background_layer=False):
+    lsr_content = json_file_to_object( f"{extracted_content_path}/Contents.json")
     width = lsr_content['properties']['canvasSize']['width']
     height = lsr_content['properties']['canvasSize']['height']
+    num_layers = len(lsr_content['layers'])
 
-    img = Image.new('RGB', (width, height))
+    combinations = [[i for i in range(1, num_layers + 1)]]
+    if all_combinations:
+        combinations = [list(subset) for subset in list(more_itertools.powerset(combinations[0])) if subset]
+        if background_layer:
+            combinations = [subset for subset in combinations if num_layers in subset]
+    
+    for combination in combinations:
+        img = Image.new('RGB', (width, height))
 
-    for layer in lsr_content['layers'][::-1]:
-        layer_name = layer['filename']
-        layer_content = json_file_to_object(
-            f"{extracted_content_path}/{layer_name}/Contents.json")
+        for layer_num in combination[::-1]:
+            layer = lsr_content['layers'][layer_num - 1]
+            layer_name = layer['filename']
+            layer_content = json_file_to_object(f"{extracted_content_path}/{layer_name}/Contents.json")
 
-        layer_width = layer_content['properties']['frame-size']['width']
-        layer_height = layer_content['properties']['frame-size']['height']
-        layer_center_x = layer_content['properties']['frame-center']['x']
-        layer_center_y = layer_content['properties']['frame-center']['y']
+            layer_width = layer_content['properties']['frame-size']['width']
+            layer_height = layer_content['properties']['frame-size']['height']
+            layer_center_x = layer_content['properties']['frame-center']['x']
+            layer_center_y = layer_content['properties']['frame-center']['y']
 
-        layer_image_set_content = json_file_to_object(
-            f"{extracted_content_path}/{layer_name}/Content.imageset/Contents.json")
+            layer_image_set_content = json_file_to_object(
+                f"{extracted_content_path}/{layer_name}/Content.imageset/Contents.json")
 
-        for image in layer_image_set_content['images']:
-            image_path = f"{extracted_content_path}/{layer_name}/Content.imageset/{image['filename']}"
-            new_img = Image.open(image_path)
-            img.paste(new_img, (int(layer_center_x - layer_width/2),
-                                int(layer_center_y - layer_height/2)), new_img)
+            for image in layer_image_set_content['images']:
+                image_path = f"{extracted_content_path}/{layer_name}/Content.imageset/{image['filename']}"
+                new_img = Image.open(image_path)
+                img.paste(new_img, (int(layer_center_x - layer_width/2), int(layer_center_y - layer_height/2)), new_img)
 
-    try:
-        os.makedirs(output_path)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-    img.save(f"{output_path}/output.jpg")
+        try:
+            os.makedirs(output_path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        
+        combination_str = ','.join([str(i) for i in combination])
+        img.save(f"{output_path}/{combination_str}.jpg")
 
 
 def cleanup(extracted_content_path=EXTRACTED_CONTENT_PATH):
@@ -63,17 +73,21 @@ def cleanup(extracted_content_path=EXTRACTED_CONTENT_PATH):
 
 @click.command()
 @click.argument('input-file-path', type=click.Path(exists=True, dir_okay=False))
-@click.option('-o', '--output-path', type=click.Path(file_okay=False, writable=True), default=OUTPUT_PATH, help="Directory to output jpg file(s) to")
-def lsr_to_jpg(input_file_path, output_path):
+@click.option('-o', '--output-path', type=click.Path(file_okay=False, writable=True), default=OUTPUT_PATH,
+              help="Directory to output jpg file(s) to")
+@click.option('-a', '--all-combinations', type=click.BOOL, default=False, help="For each combination of layers, output a jpg")
+@click.option('-b', '--background-layer', type=click.BOOL, default=False,
+              help="Always include the background layer in each combination")
+def lsr_to_jpg(input_file_path, output_path, all_combinations, background_layer):
     """Convert an lsr file to a jpg file
 
     INPUT_FILE_PATH     Must be a valid path to an lsr file
     """
     extract_lsr_content(input_file_path)
 
-    generate_image(output_path=output_path)
+    generate_images(output_path=output_path, all_combinations=all_combinations, background_layer=background_layer)
 
-    # cleanup()
+    cleanup()
 
 
 if __name__ == "__main__":
